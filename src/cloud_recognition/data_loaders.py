812c25e90fab1871e8d0c60acd32c163d2423df8
Pieter Kouyzer <paj.kouyzer@pzh.nl>
2023-12-29 12:20:46 +0000

    Restructure data preprocessing

--- src/cloud_recognition/data_loaders.py
+++ src/cloud_recognition/data_loaders.py
@@ -9,7 +9,14 @@ class FlattenedRGBImageLoader:
         self.filename = filename
         self.filepath = os.path.join(folder_path, self.filename)
 
-    def load_flattened_rgb_values(self) -> pd.DataFrame:
+    def get_rgb_df(self):
+        """
+        Loads flattened RGB values from self.filename and filters out black spots.
+        """
+        rgb_df = self._load_flattened_rgb_values()
+        return self._filter_black_spots(rgb_df)
+
+    def _load_flattened_rgb_values(self) -> pd.DataFrame:
         """
         Flattens all pixels from self.filename into a single array and returns the Red, Green and Blue bands into a pandas dataframe.
         """
@@ -22,3 +29,13 @@ class FlattenedRGBImageLoader:
                 }
             )
         return rgb_df
+
+    @staticmethod
+    def _filter_black_spots(df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Filters pixels that have 0's for Red, Green and Blue values
+        """
+        red_zero_mask = df["red"] != 0
+        green_zero_mask = df["green"] != 0
+        blue_zero_mask = df["blue"] != 0
+        return df[red_zero_mask & green_zero_mask & blue_zero_mask]
