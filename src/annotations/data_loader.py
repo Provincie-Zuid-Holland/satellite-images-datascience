65e4554d8d5c8ff42576c955869cfa4fc32fba59
Pieter Kouyzer <paj.kouyzer@pzh.nl>
2024-01-11 13:00:50 +0000

    Read multiple annotation files for Coepelduynen case

--- src/annotations/data_loader.py
+++ src/annotations/data_loader.py
@@ -1,24 +1,39 @@
+import glob
 import os
+import re
 
 import geopandas as gpd
+import pandas as pd
 
 
 def load_annotations_polygons(
-    annotations_folder: str, annotations_polygon_filename: str
+    annotations_folder: str,
+    annotations_polygon_filename_regex: str,
+    location: str,
+    images_folder: str,
 ) -> gpd.GeoDataFrame:
     """
     @param annotations_folder: folder conaintaining annotations file(s)
-    @param annotations_polygon_filename: filename of annotations geojson file
+    @param annotations_polygon_filename_regex: filename of annotations geojson file
 
     @return GeopandasDataFrame containing the polygons of all annotations for the given location
     """
-    annotations_polygons_gdf = gpd.read_file(
-        os.path.join(annotations_folder, annotations_polygon_filename)
-    )
-    if annotations_polygons_gdf.crs != "epsg:28992":
-        annotations_polygons_gdf = annotations_polygons_gdf.to_crs(epsg=28992)
-    annotations_polygons_gdf.geometry = annotations_polygons_gdf.geometry.buffer(
-        0
-    )  # transform MultiPolygon to Polygon trick
+    dfs = []
+    for filepath in glob.glob(
+        os.path.join(annotations_folder, annotations_polygon_filename_regex)
+    ):
+        df = gpd.read_file(filepath)
+        if df.crs != "epsg:28992":
+            df = df.to_crs(epsg=28992)
+        df.geometry = df.geometry.buffer(0)  # transform MultiPolygon to Polygon trick
 
-    return annotations_polygons_gdf
+        if not "name" in df.columns:
+            image_date = re.findall("[0-9]{8}", filepath)
+            image_file = glob.glob(f"{images_folder}/*{image_date}*{location}*tif")[0]
+            image_filename_without_extension = os.path.split(image_file)[-1].split(
+                ".tif"
+            )[0]
+            df["name"] = image_filename_without_extension
+
+        dfs += [df]
+    return pd.concat(dfs)
